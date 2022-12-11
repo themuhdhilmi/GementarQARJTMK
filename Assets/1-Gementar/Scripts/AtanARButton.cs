dccf03de59944aa0bec73674107aaecdd53ae04d
themuhdhilmi <hilmI@gementar.com>
2022-12-11 19:09:08 +0800

    Added new scripts.

--- Assets/1-Gementar/Scripts/AtanARButton.cs
+++ Assets/1-Gementar/Scripts/AtanARButton.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine.Playables;
+using DG.Tweening;
 
 public class AtanARButton : MonoBehaviour
 {
@@ -10,25 +12,39 @@ public class AtanARButton : MonoBehaviour
 
     public List<Texture2D> postersBody;
 
+    [Button]
     public void firstPoster()
     {
         replacePoster(postersBody[0]);
+        disableButtonPack();
     }
 
+    [Button]
     public void secondPoster()
     {
         replacePoster(postersBody[1]);
+        disableButtonPack();
     }
 
+    [Button]
     public void thirdPoster()
     {
         replacePoster(postersBody[2]);
+        disableButtonPack();
     }
 
+    [Button]
     public void fourthPoster()
     {
         replacePoster(postersBody[3]);
+        disableButtonPack();
     }  
+
+    private void mainPoster()
+    {
+        replacePoster(postersBody[4]);
+        enalbeButtonPack();
+    }
 
     private void replacePoster(Texture2D texture)
     {
@@ -38,6 +54,22 @@ public class AtanARButton : MonoBehaviour
     [Button]
     public void enalbeButtonPack()
     {
-        buttonPack.SetActive(true);
+        if(!buttonPack.activeSelf)
+        {
+            buttonPack.SetActive(true);
+            mainPoster();
+        }
+
+    }
+
+    public void disableButtonPack()
+    {
+
+        if (buttonPack.activeSelf)
+        {
+
+            buttonPack.SetActive(false);
+        }
+
     }
 }
--- Assets/1-Gementar/Scripts/AtanOnEnable.cs
+++ Assets/1-Gementar/Scripts/AtanOnEnable.cs
@@ -2,18 +2,26 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class AtanOnEnable : MonoBehaviour
 {
 
     public bool startRotate = false;
+    public bool startMainAnimation = false;
 
     private void OnEnable()
     {
         if (startRotate)
         {
-            this.transform.DOLocalRotate(new Vector3(0, 30f, 0), 0f, RotateMode.Fast);
-            this.transform.DOLocalRotate(Vector3.zero, 3f, RotateMode.Fast);
+            this.transform.DOLocalRotate(new Vector3(0, 30f, 0), 0f);
+            this.transform.DOLocalRotate(Vector3.zero, 3f);
+        }
+
+        if(startMainAnimation) 
+        {
+            PlayableDirector playableDirector = this.GetComponent<PlayableDirector>();
+            playableDirector.Play(); 
         }
     }
 
